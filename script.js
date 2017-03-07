7e289dc1d9dd54ff3cfa0c593fdff8e926caa3cd
unknown <estudiante@LAB0244.SERVERINT.LOCAL>
2017-03-07 12:26:28 -0600

    Examen de Progra

---
+++ script.js
@@ -0,0 +1,12 @@
+var BacoA = angular.module("BacoA",[]);
+Mensaje.controller("Mensaje", function($scope){  
+	
+$scope.Mensaje = 21;
+
+ })
+	
+Nombre.controller("Nombre", function($scope){
+$scope.Nombre = "Eddy";
+
+});
+
